3729e0e41ef19715b5a2dece9d8794a28ccacc7e
unknown <sifatniloy18@gmail.com>
2022-08-29 22:17:30 -1200

    box blur handled

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
@@ -6,10 +7,20 @@ const Login = () => {
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>This is login</h2>
-                <div className='input-group'>
-                    <label htmlFor="">Email</label>
-                    <input type="email" name="email" id="" />
-                </div>
+                <form action="">
+                    <div className='input-group'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" required />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="password">password</label>
+                        <input type="password" name="password" id="" required />
+                    </div>
+                    <input className='form-submit' type="submit" value="login" />
+                </form>
+                <p>
+                    new to ema john?? <Link className='form-link' to='/signup'>Create an account</Link>
+                </p>
             </div>
         </div>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmpassword, setConfirmpassword] = useState('')
+    const [error, setError] = useState('')
+
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if (password !== confirmpassword) {
+            setError('your two password did not match')
+            return;
+        }
+    }
+
+
     return (
-        <div>
-            <h2>Sign Up</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form action="">
+                    <div className='input-group'>
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="password">password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="confirm-password">confirm password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p>
+                    Already have an account? <Link className='form-link' to='/login'>log in</Link>
+                </p>
+            </div>
         </div>
     );
 };
